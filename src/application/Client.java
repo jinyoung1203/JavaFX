86979c9aa4d959bcd7664407c9cf8aa9fd986c61
ITSC <ITSC@DESKTOP-ONE4R81>
2022-12-28 21:47:30 +0900

    dobby's chatting

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,21 +1,27 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Client extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Client.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Client.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			ClientController cc = (ClientController)loader.getController();
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("JavaFX Chat");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			// 창 종료시 실행
+			primaryStage.setOnCloseRequest(event -> cc.closeAll());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
